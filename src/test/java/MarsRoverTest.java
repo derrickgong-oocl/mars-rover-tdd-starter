17796f9de0e0ec95c4ee414c77a366074332a2e3
derrickgong-oocl <derrick.gong@oocl.com>
2025-08-23 23:43:42 +0800

    implementation: create directories and first test case
    create directory and first test case

---
+++ src/test/java/MarsRoverTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MarsRoverTest {
+    @Test
+    void test_moveForward() {
+        MarsRover rover = new MarsRover(0, 0, 'N');
+        rover.execute('M');
+        assertEquals(Arrays.asList("0", "1", "N"), rover.report());
+    }
+}
