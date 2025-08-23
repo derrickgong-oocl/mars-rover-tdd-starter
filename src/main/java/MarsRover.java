a9179ab8776bc93c58bf15da516d1f70ba6a0632
derrickgong-oocl <derrick.gong@oocl.com>
2025-08-23 23:47:16 +0800

    implementaion: create local variables and constructor
    create local variables and constructor

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MarsRover {
+    private int x_position;
+    private int y_position;
+    private char direction;
+    private ArrayList<Character> rightTurnList = new ArrayList<>(Arrays.asList('N', 'E', 'S', 'W'));
+    private ArrayList<Character> leftTurnList = new ArrayList<>(Arrays.asList('N', 'W', 'S', 'E'));
+
+    public MarsRover(int x_position, int y_position, char direction) {
+        this.x_position = x_position;
+        this.y_position = y_position;
+        this.direction = direction;
+    }
 }
