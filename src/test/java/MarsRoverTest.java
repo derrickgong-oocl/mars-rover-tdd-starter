303de1f702100d50526dc731040893bd443612ff
derrickgong-oocl <derrick.gong@oocl.com>
2025-08-24 00:27:57 +0800

    Test cases: add three more test cases
    Test cases: add three more test cases

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -33,5 +33,26 @@ public class MarsRoverTest {
         assertEquals(Arrays.asList("0", "0", "W"), rover.report());
     }
 
+    @Test
+     void test_position_face_north() {
+        MarsRover rover = new MarsRover(10, 20, 'N');
+        rover.execute('M');
+        assertEquals(Arrays.asList("10", "21", "N"), rover.report());
+    }
+
+    @Test
+    void test_position_face_west() {
+        MarsRover rover = new MarsRover(10, 20, 'W');
+        rover.execute('M');
+        assertEquals(Arrays.asList("9", "20", "W"), rover.report());
+    }
+
+    @Test
+    void test_position_turn_left() {
+        MarsRover rover = new MarsRover(10, 20, 'W');
+        rover.execute('L');
+        assertEquals(Arrays.asList("10", "20", "S"), rover.report());
+    }
+
 
 }
